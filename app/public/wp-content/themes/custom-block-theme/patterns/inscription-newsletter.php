d156e32b515618970102d646502a2bbc5c45ffd2
TyphaineLeGales <typhaineLeGales@protonmail>
2023-02-02 11:27:16 +0100

    suscribe form block

---
+++ app/public/wp-content/themes/custom-block-theme/patterns/inscription-newsletter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: insciption a la newsletter
+ * Slug: custom-block-theme/newsletter-inscription
+ * Categories: featured
+ */
+?>
+
+<!-- wp:html -->
+    <form action="" method="post" class="newsletter-inscription">
+    <label for="email"></label>
+    <input type="email" id="email" pattern=".+@globex\.com" size="30" required="" placeholder="Votre email">
+    <input type="submit" value="Subscribe!" class="wp-element-button">
+    </form>
+
+<!-- /wp:html -->
